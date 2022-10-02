d095add0112b0543a632ba3e4d6a58a412fc9139
AwrelH <hiwa.awrel@gmail.com>
2022-10-02 12:28:12 +0000

    add test apply extension and function

--- test_student.py
+++ test_student.py
@@ -1,24 +1,25 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
-    
+
     @classmethod
     def setUpClass(cls):
         print('setUpClass')
-    
+
     @classmethod
     def tearDownClass(cls):
         print('tearDownClass')
-    
+
     def setUp(self):
         print('setUp')
-        self.student = Student('John', 'Doe') # refactoring the instance of student
-        
+        self.student = Student('John', 'Doe')  # refactoring the instance of student
+
     def tearDown(self):
         print('tearDown')
-        
+
     def test_full_name(self):
         print('test_full_name')
         # student = Student('John', 'Doe')  # instance of Student
@@ -34,6 +35,13 @@ class TestStudent(unittest.TestCase):
         print('test_email')
 
         self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extension(self):
+        print('test_apply_extension')
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
 
 
 if __name__ == '__main__':
